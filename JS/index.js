1906386b2198f23b83ceee73f74e07a49588b3e5
TamNguyen253 <Tamnguyen5963@gmail.com>
2025-11-30 14:53:07 +0700

    sửa lần 1 30/11/2025

--- JS/index.js
+++ JS/index.js
@@ -33,67 +33,67 @@ window.onload = countDown;
 const flashSales = products.filter((e) => e.category === "phone").slice(0, 4);
 const flashSaleList = document.getElementById("showFlashSale");
 
-for (let i = 0; i < flashSales.length; i++) {
+flashSales.forEach((e, index) => {
     flashSaleList.innerHTML += `
-    <div class="col-md-6 col-lg-3 item my-3">
-        <div class="card pt-3 bg-white">
-            <a class="link-item" data-id="${i}">
-                <div class="d-flex justify-content-center">
-                    <img
-                        src="assets/img/phones/${flashSales[i].pic}.jpg"
-                        class="card-img-top"
-                        style="width: 200px"
-                        alt="..."
-                    />
-                </div>
-                <div class="card-body">
-                    <p class="card-title fw-bold small line-clamp" style="height: 42px">
-                        ${flashSales[i].name}
-                    </p>
-                    <p class="card-text mb-0 fs-5 fw-bold text-danger">${flashSales[i].price.toLocaleString("vi-VN")}₫</p>
-                    <div class="text-decoration-line-through small text-secondary mb-3">
-                        13.990.000₫
+        <div class="col-md-6 col-lg-3 item my-3">
+            <div class="card pt-3 bg-white">
+                <a class="link-item link-prod" data-id="${index + 1}">
+                    <div class="d-flex justify-content-center">
+                        <img
+                            src="assets/img/phones/${e.pic}.jpg"
+                            class="card-img-top"
+                            style="width: 200px"
+                            alt="..."
+                        />
                     </div>
-                    <button class="btn btn-outline-primary w-100 border-2">Thêm giỏ hàng</button>
-                </div>
-            </a>
+                    <div class="card-body">
+                        <p class="card-title fw-bold small line-clamp" style="height: 42px">
+                            ${e.name}
+                        </p>
+                        <p class="card-text mb-0 fs-5 fw-bold text-danger">${e.price.toLocaleString("vi-VN")}₫</p>
+                        <div class="text-decoration-line-through small text-secondary mb-3">
+                            13.990.000₫
+                        </div>
+                        <button class="btn btn-outline-primary w-100 border-2">Thêm giỏ hàng</button>
+                    </div>
+                </a>
+            </div>
         </div>
-    </div>
-    `;
-}
+        `;
+});
 
 // ============================= show phones =============================
 const phoneData = products.filter((e) => e.category === "phone").slice(0, 4);
 const phones = document.getElementById("showPhone");
 
-for (let i = 0; i < phoneData.length; i++) {
+phoneData.forEach((e, index) => {
     phones.innerHTML += `
-    <div class="col-md-6 col-lg-3 item my-3">
-        <div class="card pt-3 bg-white">
-            <a class="link-item link-prod" data-id="${i}">
-                <div class="d-flex justify-content-center">
-                    <img
-                        src="assets/img/phones/${phoneData[i].pic}.jpg"
-                        class="card-img-top"
-                        style="width: 200px"
-                        alt="..."
-                    />
-                </div>
-                <div class="card-body">
-                    <p class="card-title fw-bold small line-clamp" style="height: 42px">
-                        ${phoneData[i].name}
-                    </p>
-                    <p class="card-text mb-0 fs-5 fw-bold text-danger"">${phoneData[i].price.toLocaleString("vi-VN")}₫</p>
-                    <div class="text-decoration-line-through small text-secondary mb-3">
-                        13.990.000₫
+        <div class="col-md-6 col-lg-3 item my-3">
+            <div class="card pt-3 bg-white">
+                <a class="link-item link-prod" data-id="${index + 1}">
+                    <div class="d-flex justify-content-center">
+                        <img
+                            src="assets/img/phones/${e.pic}.jpg"
+                            class="card-img-top"
+                            style="width: 200px"
+                            alt="..."
+                        />
                     </div>
-                    <button class="btn btn-outline-primary w-100 border-2">Thêm giỏ hàng</button>
-                </div>
-            </a>
+                    <div class="card-body">
+                        <p class="card-title fw-bold small line-clamp" style="height: 42px">
+                            ${e.name}
+                        </p>
+                        <p class="card-text mb-0 fs-5 fw-bold text-danger">${e.price.toLocaleString("vi-VN")}₫</p>
+                        <div class="text-decoration-line-through small text-secondary mb-3">
+                            13.990.000₫
+                        </div>
+                        <button class="btn btn-outline-primary w-100 border-2">Thêm giỏ hàng</button>
+                    </div>
+                </a>
+            </div>
         </div>
-    </div>
-    `;
-}
+        `;
+});
 
 // ============================= show laptops =============================
 const laptopData = products.filter((e) => e.category === "laptop").slice(0, 4);
@@ -103,7 +103,7 @@ laptopData.forEach((e, index) => {
     laptops.innerHTML += `
     <div class="col-md-6 col-lg-3 item my-3">
         <div class="card pt-3 bg-white">
-            <a class="link-item  link-prod"  data-id="${index}">
+            <a class="link-item link-prod" data-id="${index + 9}">
                 <div class="d-flex justify-content-center">
                     <img
                         src="assets/img/laptops/${e.pic}.webp"
@@ -120,7 +120,7 @@ laptopData.forEach((e, index) => {
                     <div class="text-decoration-line-through small text-secondary mb-3">
                         13.990.000₫
                     </div>
-                    <button class="btn btn-outline-primary d-inline-block w-100 border-2">Thêm giỏ hàng</button>
+                    <button class="btn btn-outline-primary w-100 border-2">Thêm giỏ hàng</button>
                 </div>
             </a>
         </div>
@@ -136,7 +136,7 @@ smartwatchData.forEach((e, index) => {
     smartwatchs.innerHTML += `
     <div class="col-md-6 col-lg-3 item my-3">
         <div class="card pt-3 bg-white">
-            <a class="link-item" data-id="${index}">
+            <a class="link-item link-prod" data-id="${index + 17}">
                 <div class="d-flex justify-content-center">
                     <img
                         src="assets/img/smartwatchs/${e.pic}.jpg"
@@ -153,7 +153,7 @@ smartwatchData.forEach((e, index) => {
                     <div class="text-decoration-line-through small text-secondary mb-3">
                         13.990.000₫
                     </div>
-                    <button class="btn btn-outline-primary d-inline-block w-100 border-2">Thêm giỏ hàng</button>
+                    <button class="btn btn-outline-primary w-100 border-2">Thêm giỏ hàng</button>
                 </div>
             </a>
         </div>
@@ -169,7 +169,7 @@ monitorData.forEach((e, index) => {
     monitors.innerHTML += `
     <div class="col-md-6 col-lg-3 item my-3">
         <div class="card pt-3 bg-white">
-            <a class="link-item link-prod" data-id="${index}">
+            <a class="link-item link-prod" data-id="${index + 25}">
                 <div class="d-flex justify-content-center">
                     <img
                         src="assets/img/monitors/${e.pic}.webp"
@@ -186,7 +186,7 @@ monitorData.forEach((e, index) => {
                     <div class="text-decoration-line-through small text-secondary mb-3">
                         13.990.000₫
                     </div>
-                    <button class="btn btn-outline-primary d-inline-block w-100 border-2">Thêm giỏ hàng</button>
+                    <button class="btn btn-outline-primary w-100 border-2">Thêm giỏ hàng</button>
                 </div>
             </a>
         </div>
